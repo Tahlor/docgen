b95e473657a249d0d42567a6e1247d8a15e65356
Taylor <tahlor@gmail.com>
2023-01-11 11:59:43 -0700

    update message

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def get_requirements(path="requirements.txt"):
     return packages
 
 setup(name='docgen',
-      version='0.0.38',
+      version='0.0.39',
       description='docgen',
       long_description= "" if not os.path.isfile("README.md") else read_md('README.md'),
       author='Taylor Archibald',
