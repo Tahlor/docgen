d610a954902e6ed0a05eee36ef50ee944814c73a
Taylor <tahlor@gmail.com>
2023-01-11 13:37:31 -0700

    update message

--- setup.py
+++ setup.py
@@ -56,12 +56,12 @@ def get_requirements(path="requirements.txt"):
     return packages
 
 setup(name='docgen',
-      version='0.0.46',
+      version='0.0.47',
       description='docgen',
       long_description= "" if not os.path.isfile("README.md") else read_md('README.md'),
       author='Taylor Archibald',
       author_email='taylor.archibald@byu.edu',
-      url='https://github.com/tahlor@/docgen',
+      url='https://github.ancestry.com/tarchibald@/docgen',
       setup_requires=['pytest-runner',],
       tests_require=['pytest','python-coveralls'],
       packages=['docgen'],
