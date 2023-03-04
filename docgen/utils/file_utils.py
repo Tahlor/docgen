b71b670a6e1e208a5e13232913dfb153d7e7d2db
Taylor <tahlor@gmail.com>
2023-03-03 17:44:27 -0700

    update message

--- docgen/utils/file_utils.py
+++ docgen/utils/file_utils.py
@@ -5,17 +5,24 @@ import shlex
 import logging
 
 logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
 
-def get_collection_files_matching_base_path(base_path):
+
+def get_collection_files_matching_base_path(base_path, exclude_base_path=False):
     """
     """
-    folder = Path(base_path).parent
-    file_name = Path(base_path).stem
+    base_path = Path(base_path)
+    folder = base_path.parent
+    file_name = base_path.stem
     matching_file_names = list(folder.glob(f"**/{file_name}*"))
+    if exclude_base_path and base_path in matching_file_names:
+        matching_file_names.remove(base_path)
     if matching_file_names:
-        logger.info(f"Found files matching {base_path}\n{matching_file_names}")
+        logger.info(f"Found files matching {str(base_path)}\n{matching_file_names}")
     else:
-        logger.info(f"No files found matching {base_path}")
+        logger.info(f"No files found matching {str(base_path)}")
     return matching_file_names
 
 def get_last_file_in_collection_matching_base_path(base_path):
@@ -27,10 +34,10 @@ def get_last_file_in_collection_matching_base_path(base_path):
     else:
         return sorted(matching_files)[-1]
 
-def append_json_files_in_collection_matching_base_path(base_path):
+def append_json_files_in_collection_matching_base_path(base_path, exclude_base_path):
     """
     """
-    matching_json_files = get_collection_files_matching_base_path(base_path)
+    matching_json_files = get_collection_files_matching_base_path(base_path, exclude_base_path)
     output_json = {}
     for f in matching_json_files:
         logger.info(f"Adding {f} to master dict")
@@ -47,6 +54,7 @@ def create_parser():
     parser = argparse.ArgumentParser(add_help=False)
     # base_file name
     parser.add_argument("--base_file", type=str, help="Path to output directory")
+    parser.add_argument("--exclude_base_file", action="store_true", help="Exclude the base path file from search")
     return parser
 
 def main(args=None):
@@ -55,7 +63,7 @@ def main(args=None):
         args = parser.parse_args()
     else:
         args = parser.parse_args(shlex.split(args))
-    json_dict = append_json_files_in_collection_matching_base_path(args.base_file)
+    json_dict = append_json_files_in_collection_matching_base_path(args.base_file, args.exclude_base_file)
     save_dict_as_json(json_dict, args.base_file)
 
 
